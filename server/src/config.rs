c505f1481787a9d370e8f4cd8e8bbdd0e5339801
aeghn <aeghn@outlook.com>
2024-08-03 22:32:10 +0800

    config: init

---
+++ server/src/config.rs
@@ -0,0 +1,30 @@
+use std::ops::Deref;
+
+use serde::Deserialize;
+
+use crate::db::MapperConfig;
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct Server {
+    pub port: u16,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct Config {
+    pub server: Option<Server>,
+    pub mapper: MapperConfig,
+}
+
+pub mod tests {
+    #[test]
+    fn test_db_deserialize() {
+        let toml_str = r#"
+        [db_config]
+        type = "sqlite"
+        filepath = "/home/123"
+    "#;
+
+        let config: super::Config = toml::from_str(toml_str).unwrap();
+        println!("{:?}", config);
+    }
+}
