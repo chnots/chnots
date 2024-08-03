2445ea16acc1faffa3405ae396c82a8576a30983
aeghn <aeghn@outlook.com>
2024-08-03 22:32:03 +0800

    feat(mapper): add MapperConfig

--- server/src/db/mod.rs
+++ server/src/db/mod.rs
@@ -2,16 +2,35 @@ pub mod postgres;
 
 use chin_tools::wrapper::anyhow::{AResult, EResult};
 use enum_dispatch::enum_dispatch;
-use postgres::Postgres;
+use postgres::{Postgres, PostgresConfig};
+use serde::Deserialize;
 
-use crate::model::{chnot::Chnot, dto::*};
+use crate::model::dto::*;
 
 #[enum_dispatch]
-pub enum DbType {
-    Postgres(Postgres),
+pub enum MapperType {
+    Postgres,
 }
 
-#[enum_dispatch(DbType)]
+#[derive(Debug, Deserialize, Clone)]
+#[serde(tag = "type")]
+pub enum MapperConfig {
+    #[serde(rename = "postgres")]
+    Postgres(PostgresConfig),
+}
+
+impl Into<AResult<MapperType>> for MapperConfig {
+    fn into(self) -> AResult<MapperType> {
+        match self {
+            MapperConfig::Postgres(config) => {
+                let pg = Postgres::new(config)?;
+                Ok(MapperType::Postgres(pg))
+            }
+        }
+    }
+}
+
+#[enum_dispatch(MapperType)]
 pub trait TableFounder {
     /// Main table.
     async fn _ensure_table_chnots(&self) -> EResult;
@@ -32,12 +51,11 @@ pub trait TableFounder {
     }
 }
 
-#[enum_dispatch(DbType)]
+#[enum_dispatch(MapperType)]
 pub trait ChnotMapper {
-    async fn chnot_overwrite(&self, chnot: Chnot) -> EResult;
+    async fn chnot_overwrite(&self, chnot: ChnotInsertionReq) -> AResult<ChnotInsertionRsp>;
     async fn chnot_delete(&self, req: ChnotDeletionReq) -> AResult<ChnotDeletionRsp>;
     async fn chnot_query(&self, req: ChnotQueryReq) -> AResult<ChnotQueryRsp>;
 }
 
-#[enum_dispatch(DbType)]
 pub trait Db: TableFounder + ChnotMapper {}
