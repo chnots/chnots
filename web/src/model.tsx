d4968fe5548d446d1b052cd9990914434b28b53f
aeghn <aeghn@outlook.com>
2024-08-04 21:20:59 +0800

    feat(web): chnots, update enum

--- web/src/model.tsx
+++ web/src/model.tsx
@@ -1,8 +1,12 @@
+export enum ChnotType {
+  MarkdownWithToent = "mdwt",
+}
+
 export interface Chnot {
   id: string;
   perm_id: string;
   content: string;
-  type: string;
+  type: ChnotType;
   delete_time?: Date;
   insert_time: Date;
   update_time: Date;
