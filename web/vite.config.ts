b26846d30ffcced0c4f306a45b1e5b52cdea8291
aeghn <aeghn@outlook.com>
2024-08-04 21:23:53 +0800

    feat(web): name alias
    1. alias __dir_name to @
    2. add @types/node

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      // !! We need to install @types/node to avoid errors(cannot find papth or __dirname).
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
