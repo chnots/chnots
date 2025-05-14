956499f8b8b6d34adfd78c27eb5fe54e6a3d6b19
aeghn <aeghn@outlook.com>
2025-05-14 23:19:01 +0800

    server: import ctable

---
+++ server/src/model/db/ctable.rs
@@ -0,0 +1,114 @@
+use core::str;
+use std::collections::HashMap;
+
+use chin_sql::{ChinSqlError, DbType, GenerateTableSql};
+use chrono::{DateTime, FixedOffset};
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub enum SqlValueType {
+    Bool,
+    I8,
+    I16,
+    I32,
+    I64,
+    F64,
+    Str,
+    FixedOffset,
+    Utc,
+    Blob,
+    Opt(Option<Box<SqlValueType>>),
+}
+
+impl ToString for SqlValueType {
+    fn to_string(&self) -> String {
+        match &self {
+            SqlValueType::Bool => "bool".into(),
+            SqlValueType::I8 => "i8".into(),
+            SqlValueType::I16 => "i16".into(),
+            SqlValueType::I32 => "i32".into(),
+            SqlValueType::I64 => "i64".into(),
+            SqlValueType::F64 => "f64".into(),
+            SqlValueType::Str => "str".into(),
+            SqlValueType::FixedOffset => "fixedoffset".into(),
+            SqlValueType::Utc => "utc".into(),
+            SqlValueType::Blob => "blob".into(),
+            SqlValueType::Opt(sql_value_type) => match sql_value_type {
+                Some(v) => format!("opt.{}", v.to_string()),
+                None => unreachable!(),
+            },
+        }
+    }
+}
+
+impl TryFrom<&str> for SqlValueType {
+    type Error = ChinSqlError;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        let res = match value {
+            "bool" => SqlValueType::Bool,
+            s => {
+                if s.starts_with("opt.") {
+                    let s = &s[4..];
+                    return SqlValueType::try_from(s);
+                } else {
+                    Err(ChinSqlError::TransformError(format!(
+                        "error getting sql value type, {}",
+                        s
+                    )))?
+                }
+            }
+        };
+
+        Ok(res)
+    }
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct CTableColumnMeta {
+    pub index: u32,
+    pub name: String,
+    pub comment: String,
+    pub stype: SqlValueType,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize, GenerateTableSql)]
+pub struct CTableMeta {
+    #[gts_type = "String"]
+    pub columns: HashMap<String, CTableColumnMeta>,
+    pub table_name: String,
+    pub table_comment: String,
+    pub create_time: DateTime<FixedOffset>,
+    pub update_time: Option<DateTime<FixedOffset>>,
+    pub delete_time: Option<DateTime<FixedOffset>>,
+    pub real_table: bool,
+}
+
+macro_rules! type_table {
+    ($suffix:tt, $data_type:tt, $(#[$attr:meta])*) => {
+        #[derive(Clone, Debug, Serialize, Deserialize, GenerateTableSql)]
+        pub struct  $suffix {
+            pub table_name: String,
+            pub col_idx: i32,
+            pub row_idx: i32,
+            $(#[$attr])*
+            pub cell_data: $data_type,
+            pub insert_time: DateTime<FixedOffset>,
+            pub delete_time: Option<DateTime<FixedOffset>>,
+            pub namespace: String
+        }
+
+        
+    };
+
+    ($suffix:tt, $data_type:ty, $(#[$attr:meta])*) => {
+        type_table!($suffix, $data_type, $(#[$attr])*);
+    };
+}
+
+type_table!(CTableCellStr1024, String, #[gts_length = 1024]);
+type_table!(CTableCellText, String,);
+type_table!(CTableCellInteger, i64,);
+
+type DateFixed = DateTime<FixedOffset>;
+type_table!(CTableCellDate, DateFixed, #[gts_type = "DateTime<FixedOffset>"]);
---
+++ server/src/model/dto/ctable.rs
@@ -0,0 +1,91 @@
+use serde::{Deserialize, Serialize};
+
+use crate::model::db::ctable::{
+    CTableCellDate, CTableCellInteger, CTableCellStr1024, CTableCellText, CTableMeta,
+};
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub enum CTableCell {
+    String(CTableCellStr1024),
+    Text(CTableCellText),
+    Integer(CTableCellInteger),
+    Date(CTableCellDate),
+}
+
+macro_rules! into_cell {
+    ($sub_type:tt, $etype:tt) => {
+        impl From<$sub_type> for CTableCell {
+            fn from(value: $sub_type) -> Self {
+                Self::$etype(value)
+            }
+        }
+    };
+}
+
+into_cell! {CTableCellStr1024, String}
+into_cell! {CTableCellText, Text}
+into_cell! {CTableCellInteger, Integer}
+into_cell! {CTableCellDate, Date}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct CTableOverwriteMetaReq {
+    pub meta: CTableMeta,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct CTableOverwriteMetaRsp {}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct CTableOverwriteRowReq {
+    pub row: Vec<CTableCell>,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct CTableOverwriteRowRsp {}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+
+pub struct CTableOverwriteCellReq {
+    pub cell: CTableCell,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+
+pub struct CTableOverwriteCellRsp {}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+
+pub struct CTableQueryRowReq {
+    pub table_name: String,
+    pub row_index: i64,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+
+pub struct CTableQueryRowRsp {
+    pub row: Vec<CTableCell>,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+
+pub struct CTableQueryTableMetaReq {
+    pub table_name: String,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+
+pub struct CTableQueryTableMetaRsp {
+    pub meta: CTableMeta,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+
+pub struct CTableQueryTableDataReq {
+    pub table_name: String,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+
+pub struct CTableQueryTableDataRsp {
+    pub cells: Vec<CTableCell>,
+}
