2223d9a49203c6bfffcb459a4e4c4d685011e056
aeghn <aeghn@outlook.com>
2025-06-18 08:02:45 +0800

    web: misc

--- web/src/krate/tool/excalidraw/page/excalidraw.tsx
+++ web/src/krate/tool/excalidraw/page/excalidraw.tsx
@@ -1,10 +1,13 @@
+import { SaveState } from "@/common/types";
 import ExcalidrawContainer from "../component/excalidraw-container";
 
 import "@excalidraw/excalidraw/index.css";
 
 
 const ExcalidrawPage = () => {
-  return <ExcalidrawContainer></ExcalidrawContainer>;
+  return <ExcalidrawContainer onSetSaveState={function(state: SaveState): void {
+    throw new Error("Function not implemented.");
+  } }></ExcalidrawContainer>;
 };
 
 export default ExcalidrawPage;
