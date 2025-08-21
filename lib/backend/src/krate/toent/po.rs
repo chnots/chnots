520fb015a496f6cb9592a15d0e207e2d15fd0d81
aeghn <aeghn@outlook.com>
2025-08-21 21:10:18 +0800

    backend: toent logic

--- lib/backend/src/krate/toent/po.rs
+++ lib/backend/src/krate/toent/po.rs
@@ -7,16 +7,50 @@
 /// I merged them into the word "toent."
 use chin_sql::{GenerateTableSchema, SqlValue, str_type::Varchar, time_type::TID};
 
+use enum_iterator::Sequence;
 use serde::{Deserialize, Serialize};
 
-use crate::krate::toent::logic::{EventBuilder, todoevent::TodoEvent};
+use crate::{
+    enum_common_funcs,
+    krate::toent::logic::{EventBuilder, todoevent::TodoEvent},
+};
+
+#[derive(Debug, Clone, Sequence)]
+pub(crate) enum TimeEventAction {
+    Skip,
+}
+
+impl TimeEventAction {
+    pub(crate) fn as_static_str(&self) -> &'static str {
+        match self {
+            TimeEventAction::Skip => "skip",
+        }
+    }
+}
+
+enum_common_funcs! {TimeEventAction}
+
+fn time_event_action_to_sql(this: TimeEventAction) -> Varchar<40> {
+    this.as_static_str().try_into().unwrap()
+}
 
 #[derive(Debug, Clone, Serialize, Deserialize, GenerateTableSchema)]
 pub(crate) struct ToentTimeEventInst {
+    #[gts_key]
+    #[gts_type = "i64"]
+    chnot_otid: TID,
+
+    #[gts_primary]
     chnot_block_id: Varchar<100>,
 
+    #[gts_primary]
     target_time_utc: i64,
+
     alert_time_utc: i64,
+
+    #[gts_type = "Varchar<40>"]
+    #[gts_tosql = "time_event_action_to_sql"]
+    action: TimeEventAction,
 
     #[gts_unique]
     #[gts_type = "i64"]
---
+++ lib/backend/src/prelude.rs
@@ -0,0 +1,8 @@
+pub(crate) use anyhow::Context as _;
+pub(crate) use chin_sql::str_type::Varchar;
+pub(crate) use chin_tools::AError;
+pub(crate) use chin_tools::AResult;
+pub(crate) use chin_tools::EResult;
+pub(crate) use chin_tools::SharedStr;
+
+pub(crate) use crate::model::dto::KReq;
