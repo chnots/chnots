a1590329757836113136946aadd179d210078da4
aeghn <aeghn@outlook.com>
2025-11-24 20:57:12 +0800

    chore(web): remove use-debug-changed

--- web/src/hooks/use-debug-changed.ts
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-
-const useDebugChanged = (value: any, title?: string) => {
-  useEffect(() => {
-    console.log("> changed,", title, value);
-  }, [value]);
-};
-
-export default useDebugChanged;
