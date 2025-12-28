48481861a81cec49d38b7ea216531f19a894580c
aeghn <aeghn@outlook.com>
2025-12-28 23:41:50 +0800

    refactor: remove mind-elixir-react

--- lib/mind-elixir-react/rstest.config.ts
+++
@@ -1,8 +0,0 @@
-import { pluginReact } from '@rsbuild/plugin-react';
-import { defineConfig } from '@rstest/core';
-
-export default defineConfig({
-  testEnvironment: 'jsdom',
-  setupFiles: ['./rstest.setup.ts'],
-  plugins: [pluginReact()],
-});
