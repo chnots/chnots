5d525c0511a19164a06b6aef266f159a93484eec
wangzh <wzhchin@outlook.com>
2026-03-01 08:34:44 +0800

    feat(tauri): enable log plugin in release builds

--- tauri/src-tauri/src/lib.rs
+++ tauri/src-tauri/src/lib.rs
@@ -29,13 +29,11 @@ pub fn run() {
                 },
             };
 
-            if cfg!(debug_assertions) {
-                app.handle().plugin(
-                    tauri_plugin_log::Builder::default()
-                        .level(log::LevelFilter::Info)
-                        .build(),
-                )?;
-            }
+            app.handle().plugin(
+                tauri_plugin_log::Builder::default()
+                    .level(log::LevelFilter::Info)
+                    .build(),
+            )?;
 
             tauri::async_runtime::spawn(async move {
                 chnots_core::run(config).await.unwrap();
