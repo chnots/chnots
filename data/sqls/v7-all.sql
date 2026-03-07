39f02939fc54e09ad038b6e4436085992eb6e230
wangzh <wzhchin@outlook.com>
2026-03-07 18:50:07 +0800

    chore(sql): update v7 toent tables without IF NOT EXISTS

--- data/sqls/v7-all.sql
+++ data/sqls/v7-all.sql
@@ -1,64 +1,55 @@
 CREATE TABLE
-    IF NOT EXISTS toent_todo (
+    toent_todo (
         otid BIGINT NOT NULL,
-        todo_priority BIGINT,
         todo_state VARCHAR(30),
-        todo_closed BOOLEAN NOT NULL,
+        todo_priority BIGINT,
+        alert_tid BIGINT,
+        start_tid BIGINT,
+        end_tid BIGINT,
+        timezone BIGINT,
+        closed BOOLEAN,
+        tid BIGINT NOT NULL,
+        note TEXT,
+        PRIMARY KEY (otid),
+        UNIQUE (tid)
+    );
+
+CREATE TABLE
+    toent_todo_hist (
+        otid BIGINT NOT NULL,
+        todo_state VARCHAR(30),
+        todo_priority BIGINT,
+        alert_tid BIGINT,
+        start_tid BIGINT,
+        end_tid BIGINT,
+        timezone BIGINT,
+        closed BOOLEAN,
+        tid BIGINT NOT NULL,
+        note TEXT,
+        UNIQUE (tid)
+    );
+
+CREATE TABLE
+    toent_event (
+        otid BIGINT NOT NULL,
+        event_defi TEXT NOT NULL,
+        start_time BIGINT,
+        start_timezone BIGINT,
+        end_time BIGINT,
+        end_timezone BIGINT,
         tid BIGINT NOT NULL,
         PRIMARY KEY (otid),
         UNIQUE (tid)
     );
 
 CREATE TABLE
-    IF NOT EXISTS toent_todo_hist (
-        otid BIGINT NOT NULL,
-        todo_priority BIGINT,
-        todo_state VARCHAR(30),
-        todo_closed BOOLEAN NOT NULL,
-        tid BIGINT NOT NULL,
-        UNIQUE (tid)
-    );
-
-CREATE TABLE
-    IF NOT EXISTS toent_event (
+    toent_event_hist (
         otid BIGINT NOT NULL,
         event_defi TEXT NOT NULL,
-        tid BIGINT NOT NULL,
-        PRIMARY KEY (otid),
-        UNIQUE (tid)
-    );
-
-CREATE TABLE
-    IF NOT EXISTS toent_event_hist (
-        otid BIGINT NOT NULL,
-        event_defi TEXT NOT NULL,
-        tid BIGINT NOT NULL,
-        UNIQUE (tid)
-    );
-
-CREATE TABLE
-    IF NOT EXISTS toent_inst (
-        otid BIGINT NOT NULL,
-        timezone VARCHAR(10),
-        naive_time VARCHAR(30) NOT NULL,
-        target_status VARCHAR(30),
-        note TEXT,
-        alert_tid BIGINT,
-        target_tid BIGINT NOT NULL,
-        tid BIGINT NOT NULL,
-        PRIMARY KEY (otid, target_tid),
-        UNIQUE (tid)
-    );
-
-CREATE TABLE
-    IF NOT EXISTS toent_inst_hist (
-        otid BIGINT NOT NULL,
-        timezone VARCHAR(10),
-        naive_time VARCHAR(30) NOT NULL,
-        target_status VARCHAR(30),
-        note TEXT,
-        alert_tid BIGINT,
-        target_tid BIGINT NOT NULL,
+        start_time BIGINT,
+        start_timezone BIGINT,
+        end_time BIGINT,
+        end_timezone BIGINT,
         tid BIGINT NOT NULL,
         UNIQUE (tid)
     );
